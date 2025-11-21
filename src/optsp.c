1aa088a7e4b042961fb1126c6a0bc612cd9bdb23
Bill Waller <billxwaller@gmail.com>
2025-11-21 15:27:42 -0600

    Delete src/optsp.c

--- src/optsp.c
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-#include <stdio.h>
-
-void dump_opts_by_use(char *, char *);
-
-int main(int argc, char **argv) {
-    dump_opts_by_use("usage: {menu|pick|form|view}", "mpfv");
-    printf("\n");
-}
