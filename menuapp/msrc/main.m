494cb2d27a68e4771f97b50d827ffad7a667ce85
Bill Waller <billxwaller@gmail.com>
2025-12-25 09:55:57 -0600

    Can't use treesitter with main.m (no parser)

--- menuapp/msrc/main.m
+++ menuapp/msrc/main.m
@@ -7,8 +7,6 @@
 !exec rsh
 :     Test Curses Keys
 !ckeys
-:     View a Source File
-!pick -i ./ls -n 1 -c bat
 :     Installment Loan Calculations
 !form iloan.f -i iloan.dat -c iloan -o iloan.dat
 :     Listener Research
@@ -19,9 +17,11 @@
 !form -d fields.f -i fields.dat -o fields.dat
 :     Edit .c Files in Current Directory
 !pick -S "lf ./ .*\.c$" -n 1 -c vi -T "Select File to Edit"
+:     View a C File
+!view -L 50 -C 80 -S "tree-sitter highlight init_view.c"
 :     View Data Types Help File
 !view -L 17 -C 45 -T "Data Types" /home/bill/menuapp/data/fields.f
 :     Menu Description
-!view -L 36 -C 74 main.m
+!view -L 30 -C 78 -S "bat -l Crystal -f /home/bill/menuapp/msrc/main.m"
 :     Exit Applications
 !return
