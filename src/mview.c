439158436cf228adc1141b7795fe57aa871dab53
Bill Waller <billxwaller@gmail.com>
2025-12-25 16:48:46 -0600

    View screen geometry corrected

--- src/mview.c
+++ src/mview.c
@@ -30,19 +30,49 @@ int mview(Init *init, int argc, char **argv, int lines, int cols, int begy,
         }
         exit(EXIT_SUCCESS);
     }
-    if (!lines || !cols) {
-        view->lines = LINES * 3 / 4;
-        view->cols = COLS * 3 / 4;
-    } else {
-        view->lines = lines;
-        view->cols = cols;
-    }
-    if (!begy || !begx) {
-        begy = (LINES - view->lines) / 2;
-        begx = (COLS - view->cols) / 2;
-    }
-    view->begy = begy;
-    view->begx = begx;
+
+    if (init->start_cmd[0] != '\0')
+        strip_quotes(init->start_cmd);
+    if (init->title[0] == '\0')
+        strnz__cpy(init->title, init->start_cmd, MAXLEN - 1);
+    else
+        strip_quotes(init->title);
+
+    /*  ╭───────────────────────────────────────────────────────────────╮
+        │ view->lines     2/3                                           │
+        ╰───────────────────────────────────────────────────────────────╯ */
+    if (init->lines > LINES - 3)
+        init->lines = LINES - 3;
+    else if (init->lines == 0)
+        init->lines = LINES * 3 / 4;
+    view->lines = init->lines;
+
+    /*  ╭───────────────────────────────────────────────────────────────╮
+        │ view->cols      2/3                                           │
+        ╰───────────────────────────────────────────────────────────────╯ */
+    if (init->cols > COLS - 3)
+        init->cols = COLS - 3;
+    else if (init->cols == 0)
+        init->cols = COLS * 3 / 4;
+    view->cols = init->cols;
+    /*  ╭───────────────────────────────────────────────────────────────╮
+        │ view->cols      1/5      top margin                           │
+        ╰───────────────────────────────────────────────────────────────╯ */
+    if (init->begy + lines > LINES - 4)
+        init->begy = LINES - lines - 2;
+    else if (init->begy == 0)
+        init->begy = (LINES - lines) / 5;
+    view->begy = init->begy;
+
+    /*  ╭───────────────────────────────────────────────────────────────╮
+        │ view->cols      1/5      left margin                          │
+        ╰───────────────────────────────────────────────────────────────╯ */
+    if (init->begx + cols > COLS - 4)
+        init->begx = COLS - cols - 2;
+    else if (init->begx == 0)
+        init->begx = (COLS - cols) / 8;
+    view->begx = init->begx;
+
     view->f_full_screen = false;
     if (!init_view_boxwin(init, title)) {
         view_file(init);
