f4f16aae6daf538e09046d1f15447e2e3b1d74b1
Bill Waller <billxwaller@gmail.com>
2026-01-27 22:56:00 -0600

    Corrected issue with italics attribute

--- src/end_pgm.h
+++
@@ -1,9 +0,0 @@
-#define __end_pgm                                                              \
-    {                                                                          \
-        static void end_pgm(void) {                                            \
-            destroy_init(init);                                                \
-            win_del();                                                         \
-            destroy_curses();                                                  \
-            restore_shell_tioctl();                                            \
-            exit(EXIT_FAILURE);                                                \
-        }
