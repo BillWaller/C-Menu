6005693c4ddb36ccf17a086b4e3ffdb3ed159d3d
Bill Waller <billxwaller@gmail.com>
2026-02-03 21:30:17 -0600

    Calculate Window Size if zero

--- src/mview.c
+++ src/mview.c
@@ -13,38 +13,44 @@ int mview(Init *init, int argc, char **argv) {
         view = new_view(init, argc, argv);
     else
         view = init->view;
+    ///  ╭───────────────────────────────────────────────────────────╮
+    ///  │ view->lines     3/4                                       │
+    ///  ╰───────────────────────────────────────────────────────────╯
+    if (init->lines == 0)
+        view->lines = LINES * 3 / 4;
+    else
+        view->lines = init->lines;
+    if (view->lines > LINES - 3)
+        view->lines = LINES - 3;
 
     ///  ╭───────────────────────────────────────────────────────────╮
-    ///  │ view->lines     2/3                                       │
+    ///  │ view->cols      3/4                                       │
     ///  ╰───────────────────────────────────────────────────────────╯
-    if (init->lines > LINES - 3)
-        init->lines = LINES - 3;
-    init->lines = LINES * 3 / 4;
-    view->lines = init->lines;
+    if (init->cols == 0)
+        view->cols = COLS * 3 / 4;
+    else
+        view->cols = init->cols;
+    if (view->cols > COLS - 3)
+        view->cols = COLS - 3;
+    ///  ╭───────────────────────────────────────────────────────────╮
+    ///  │ view->begy      1/5      top margin                       │
+    ///  ╰───────────────────────────────────────────────────────────╯
+    if (init->begy == 0)
+        view->begy = (LINES - view->lines) / 5;
+    else
+        view->begy = init->begy;
+    if (view->begy + view->lines > LINES - 4)
+        view->begy = LINES - view->lines - 2;
 
     ///  ╭───────────────────────────────────────────────────────────╮
-    ///  │ view->cols      2/3                                       │
+    ///  │ view->begx      1/5      left margin                      │
     ///  ╰───────────────────────────────────────────────────────────╯
-    if (init->cols > COLS - 3)
-        init->cols = COLS - 3;
-    init->cols = COLS * 3 / 4;
-    view->cols = init->cols;
-    ///  ╭───────────────────────────────────────────────────────────╮
-    ///  │ view->cols      1/5      top margin                       │
-    ///  ╰───────────────────────────────────────────────────────────╯
-    if (init->begy + view->lines > LINES - 4)
-        init->begy = LINES - view->lines - 2;
-    init->begy = (LINES - view->lines) / 5;
-    view->begy = init->begy;
-
-    ///  ╭───────────────────────────────────────────────────────────╮
-    ///  │ view->cols      1/5      left margin                      │
-    ///  ╰───────────────────────────────────────────────────────────╯
-    if (init->begx + view->cols > COLS - 4)
-        init->begx = COLS - view->cols - 2;
-    init->begx = (COLS - view->cols) / 2;
-    view->begx = init->begx;
-
+    if (init->begx == 0)
+        view->begx = (COLS - view->cols) / 5;
+    else
+        view->begx = init->begx;
+    if (view->begx + view->cols > COLS - 4)
+        view->begx = COLS - view->cols - 2;
     view->f_full_screen = false;
     if (!init_view_boxwin(init, init->title)) {
         view_file(init);
