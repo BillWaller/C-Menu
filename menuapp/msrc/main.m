060c035219d723660f96859f375f0c45135d0600
Bill Waller <billxwaller@gmail.com>
2026-02-11 18:02:41 -0600

    Documentation Updates

--- menuapp/msrc/main.m
+++ menuapp/msrc/main.m
@@ -16,13 +16,13 @@
 :     Form Data Types
 !form -d fields.f -i fields.dat -o fields.dat
 :     Edit .c Files in Current Directory
-!pick -S project_src -M -c vi -T "Project Tree - Select File to Edit"
+!pick -S project_src -c nvim -T "Project Tree - Select File to Edit"
 :     View CMenu Source with Tree-Sitter
-!pick -S project_src -n 1 -T "Select Project File to Highlight" -c "view -S \"tree-sitter highlight %%\""
+!pick -S project_src -n 1 -T "Select Project File to Highlight" -c "view -L 60 -C 70 -S \"tree-sitter highlight %%\""
 :     View Data Types Help File
-!view -L 40 -C 80 -T "Data Types" /home/bill/menuapp/help/fields.hlp
+!view -T "Data Types" /home/bill/menuapp/help/fields.hlp
 :     Menu Description
-!view -L 30 -C 78 -S "bat --theme ansi -l Crystal -f /home/bill/menuapp/msrc/main.m"
+!view -S "bat --theme ansi -l Crystal -f /home/bill/menuapp/msrc/main.m"
 :     Help
 !view view -L 54 -C 84 -S "optsp.sh"
 :     Exit Applications
