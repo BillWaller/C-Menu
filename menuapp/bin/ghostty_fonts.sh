8a7412261fc0107430751b2820c86e099fc77f5c
Bill Waller <billxwaller@gmail.com>
2026-03-09 17:55:46 -0500

    Modified scripts to sort output

--- menuapp/bin/ghostty_fonts.sh
+++ menuapp/bin/ghostty_fonts.sh
@@ -4,6 +4,6 @@ G=$(id -gn)
 fonts_dir="$HOME"/.config/ghostty/fonts
 rsh -c "chown -R $U:$G $fonts_dir"
 cd "$fonts_dir"
-for font in $(lf -d 1 -t f -e '.*default_font$' . '.*'); do
+for font in $(lf -d 1 -t f -e '.*default_font$' . '.*' | sort); do
     echo $(basename "$font")
 done
