5c117694a6ebf52ebc8f9874c84d93ac955ea971
BillWaller <billxwaller@gmail.com>
2026-04-30 11:50:08 -0500

    Remove hard-coded paths from main.m

--- menuapp/msrc/main.m
+++ menuapp/msrc/main.m
@@ -23,9 +23,9 @@
 :     View Source with Tree-Sitter
 !pick -S src -n 1 -T "Select Source File to Highlight" -c "view -L 60 -C 85 -S \"ts_hl.sh %%\""
 :     View Data Types Help File
-!view -Nf -T "Data Types" /home/bill/menuapp/help/fields.hlp
+!view -Nf -L47 -C85 -S "bat --theme ansi -l Crystal -f ~/menuapp/help/fields.hlp"
 :     Menu Description With Bat Syntax Highlighting
-!view -Nf -L30 -C 75 -S "bat --theme ansi -l Crystal -f /home/bill/menuapp/msrc/main.m"
+!view -Nf -L39 -C85 -S "bat --theme ansi -l Crystal -f ~/menuapp/msrc/main.m"
 :     View C-Menu Command Line Options
 !view -Nf -L66 -C75 ~/menuapp/help/menu.help
 :     View Highlighted view_engine.c
