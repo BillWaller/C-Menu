b9e49fe9b2947f0ffe187999b415cf6e832bbf3e
BillWaller <billxwaller@gmail.com>
2026-09-13 20:49:23 -0500

    EXPERIMENTAL: lf_tests.sh Test script for testing and comparing lf4.c

--- src/lf_tests.sh
+++ src/lf_tests.sh
@@ -5,40 +5,47 @@ directory="/home/bill"
 echo "## Large Directory - 500,000+ files"
 echo
 # ------------------------------------------------------------
-echo "### lf4"
-echo
-/bin/time -o time.out ./lf4 -H -T7 "$directory" >lf4.out 2>/dev/null
-sort lf4.out | sed 's/\/$//' >lf4b.out
+echo "### find"
+echo find
+/bin/time -o time.out find "$directory" >find.out 2>/dev/null
 grep -v "^Command" time.out
-echo
-lf_found=$(wc -l lf4b.out | sed 's/ .*//')
-echo "lf4 found $lf_found files"
+echo sort
+/bin/time -o time.out sh -c 'LC_ALL=C sort --parallel=7 -S 4G find.out' | sed 's/^\///
+/^\.$/d' >findb.out
+grep -v "^Command" time.out
+found=$(wc -l findb.out | sed 's/ .*//')
+echo "find found $found files"
 echo
 echo "---"
 echo
 # ------------------------------------------------------------
 echo "### fd"
 echo
-/bin/time -o time.out fd . -H -I "$directory" >fd4.out 2>/dev/null
-sort fd4.out | sed 's/^\///
-    s/\/$//' >fd4b.out
+echo fd
+/bin/time -o time.out fd . -H -I "$directory" >fd.out 2>/dev/null
 grep -v "^Command" time.out
-echo
-fd_found=$(wc -l fd4b.out | sed 's/ .*//')
-echo "fd found $fd_found files"
+echo sort
+/bin/time -o time.out sh -c 'LC_ALL=C sort --parallel=7 -S 4G fd.out' | sed 's/^\///
+    s/\/$//' >fdb.out
+grep -v "^Command" time.out
+found=$(wc -l fdb.out | sed 's/ .*//')
+echo "fd found $found files"
 echo
 echo "---"
 echo
 # ------------------------------------------------------------
-echo "### find"
-/bin/time -o time.out find "$directory" >find2.out
-sed 's/^\.\///
-    /^\.$/d' find2.out | sort | grep -v "^$directory$" >find2b.out
+echo "### lf4"
 echo
+echo lf4
+/bin/time -o time.out ./lf4 -H -T7 "$directory" >lf4.out 2>/dev/null
 grep -v "^Command" time.out
-echo
-find_found=$(wc -l find2b.out | sed 's/ .*//')
-echo "find found $find_found files"
+echo sort
+/bin/time -o time.out sh -c 'LC_ALL=C sort --parallel=7 -S 4G lf4.out' |
+    sed 's/^\///
+        s/\/$//' >lf4b.out
+grep -v "^Command" time.out
+found=$(wc -l lf4b.out | sed 's/ .*//')
+echo "lf4 found $found files"
 echo
 echo "---"
 echo
