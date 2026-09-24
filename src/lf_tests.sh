e6a09c882c61d89ce46a230c11fdfa5c9d542130
BillWaller <billxwaller@gmail.com>
2026-09-24 11:28:02 -0500

    FIXED minor error in lf_tests.sh

--- src/lf_tests.sh
+++ src/lf_tests.sh
@@ -22,6 +22,6 @@ echo
 echo "### lf"
 echo
 echo ./lf -H "$directory"
-/bin/time -o time.out ./lf2 -H -D4 "$directory" | wc -l >lf.out 2>lf.errors
+/bin/time -o time.out ./lf -H "$directory" | wc -l >lf.out 2>/dev/null
 grep -v "^Command" time.out
 echo "lf found "$(cat lf.out)" files"
